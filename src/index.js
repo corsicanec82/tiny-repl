04cf492266351e5ef06f56ce2fa8eb0601983a85
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2020-04-12 21:42:38 +0300

    embed codemirror

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 // @ts-check
 
-document
-  .getElementById('welcome')
-  .textContent = 'Welcome to application!';
+import codeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/javascript/javascript.js';
+import 'codemirror/mode/php/php.js';
+import 'codemirror/addon/edit/closebrackets.js';
+import 'codemirror/addon/selection/active-line.js';
+import 'codemirror/addon/edit/matchbrackets.js';
+import 'codemirror/addon/scroll/scrollpastend.js';
+
+codeMirror(document.getElementById('editor'), {
+  lineNumbers: true,
+  tabSize: 2,
+  // mode: 'text/x-php',
+  mode: 'javascript',
+  autoCloseBrackets: true,
+  styleActiveLine: true,
+  matchBrackets: true,
+  scrollPastEnd: true,
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const mode = process.env.NODE_ENV || 'development';
 
@@ -16,6 +18,14 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
+  plugins: [
+    new CompressionPlugin(),
+    // new BundleAnalyzerPlugin(),
+  ],
 };
